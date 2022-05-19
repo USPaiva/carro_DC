331034e7abd4680dda4c68bae848f65f3dc2f6e4
Uriel da Conceição <30707490+USPaiva@users.noreply.github.com>
2022-05-19 18:21:15 -0300

    Add files via upload

---
+++ carro_DC_1.5v.ino
@@ -0,0 +1,76 @@
+#include <Ultrasonic.h>
+#define LED 6
+//sensor de distancia
+#define echo 30
+#define trig 31
+//motor1 é direita
+//motor2 é esquerda
+#define motorPin1 2 // motor1 Input 1
+#define motorPin2 3 // motor1 Input 2
+#define motorPin3 8 // motor2 Input 1
+#define motorPin4 7 // motor2 Input 2
+// Pinos de conexao do sensor de cor TCS230 da direita
+#define l_out 23
+#define l_s1 22
+#define l_s2 25
+#define l_s0 24
+#define l_s3 26
+// Pinos de conexao do sensor de cor TCS230 da esquerda
+#define r_s0 53
+#define r_s1 52
+#define r_s2 50
+#define r_s3 51
+#define r_out 48
+// Valores adquiridos a cada loop
+int l_red = 0;
+int l_green = 0;
+int l_blue = 0; 
+int r_red = 0;
+int r_green = 0; 
+int r_blue = 0;
+// Contagem
+int count = 0;
+//Inicializa o sensor nos pinos definidos acima
+Ultrasonic ultrasonic(trig, echo);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("v1.5 meio testada");
+
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4 , OUTPUT);
+}
+void ultrason(){
+   //Le as informacoes do sensor, em cm e pol
+  float cmMsec, inMsec;
+  long microsec = ultrasonic.timing();
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  inMsec = ultrasonic.convert(microsec, Ultrasonic::IN);
+  //Exibe informacoes no serial monitor
+  Serial.print("Distancia em cm: ");
+  Serial.print(cmMsec);
+  Serial.print(" - Distancia em polegadas: ");
+  Serial.println(inMsec);
+ if (cmMsec < 11) {
+   Serial.println("obstaculo");
+   digitalWrite(motorPin1, LOW); // set Input 1 of the L293D low
+   digitalWrite(motorPin2, HIGH);
+   digitalWrite(motorPin3, LOW); // set Input 1 of the L293D low
+   digitalWrite(motorPin4, HIGH);
+   Serial.println("-----");
+ }
+}
+void loop() {
+  count++;
+ analogWrite(12, 460);
+ analogWrite(11, 460); 
+   ultrason();
+   getColorsFromLeftSensor();
+   getColorsFromRightSensor();
+   BothColors();
+  printColorsOfLeftSensor();
+  printColorsOfRightSensor(); 
+}
+
---
+++ voids.ino
@@ -0,0 +1,103 @@
+void getColorsFromLeftSensor() {
+  // (?!) Nao faço a minima ideia de como isso funciona
+  digitalWrite(l_s2, LOW);
+  digitalWrite(l_s3, LOW);
+  l_red = pulseIn(l_out, digitalRead(l_out) == HIGH ? LOW : HIGH);
+  digitalWrite(l_s3, HIGH);
+  l_blue = pulseIn(l_out, digitalRead(l_out) == HIGH ? LOW : HIGH);
+  digitalWrite(l_s2, HIGH);
+  l_green = pulseIn(l_out, digitalRead(l_out) == HIGH ? LOW : HIGH);
+}
+
+void getColorsFromRightSensor() {
+  // (?!) Nao faço a minima ideia de como isso funciona
+  digitalWrite(r_s2, LOW);
+  digitalWrite(r_s3, LOW);
+  r_red = pulseIn(r_out, digitalRead(r_out) == HIGH ? LOW : HIGH);
+  digitalWrite(r_s3, HIGH);
+  r_blue = pulseIn(r_out, digitalRead(r_out) == HIGH ? LOW : HIGH);
+  digitalWrite(r_s2, HIGH);
+  r_green = pulseIn(r_out, digitalRead(r_out) == HIGH ? LOW : HIGH);
+}
+
+
+void printColorsOfLeftSensor() {
+  Serial.println(String(count) + String("- SENSOR ESQUERDO"));
+  Serial.println(String("RED:   ") + String(l_red));
+  Serial.println(String("GREEN: ") + String(l_green));
+  Serial.println(String("BLUE:  ") + String(l_blue));
+
+  if (l_blue > 15 && l_blue <= 30 && l_blue > 15 && l_green > 15 && l_red > 15) {
+    Serial.println("preto");
+    digitalWrite(motorPin1, LOW); // set Input 1 of the L293D low
+    digitalWrite(motorPin2, LOW); // set Input 2 of the L293D high
+    digitalWrite(motorPin4, HIGH); // set Input 1 of the L293D low
+   digitalWrite(motorPin3, LOW);
+   delay(50);
+   digitalWrite(motorPin4, LOW); // set Input 1 of the L293D low
+   digitalWrite(motorPin3, LOW);
+  } else if (l_blue <= 15 && l_red <= 15 && l_green <= 15) {
+    Serial.println("branco");
+   digitalWrite(motorPin4, HIGH); // set Input 1 of the L293D low
+   digitalWrite(motorPin3, LOW);
+   delay(50);
+   digitalWrite(motorPin4, LOW); // set Input 1 of the L293D low
+   digitalWrite(motorPin3, LOW);
+  } else if (l_blue > 30) {
+    Serial.println("Verde");
+  digitalWrite(motorPin4, LOW); // set Input 1 of the L293D low
+   digitalWrite(motorPin3, LOW);
+  }
+
+  Serial.println("-----");
+}
+
+void printColorsOfRightSensor() {
+  Serial.println(String(count) + String("- SENSOR DIREITO"));
+  Serial.println(String("RED: ") + String(r_red));
+  Serial.println(String("GREEN: ") + String(r_green));
+  Serial.println(String("BLUE: ") + String(r_blue));
+
+  if (r_green > 16 && r_green < 30) {
+    Serial.println("preto");
+  digitalWrite(motorPin4, LOW); // set Input 1 of the L293D low
+   digitalWrite(motorPin3, LOW);
+   digitalWrite(motorPin1, HIGH); // set Input 1 of the L293D low
+   digitalWrite(motorPin2, LOW);
+   delay(50);
+   digitalWrite(motorPin1, LOW); // set Input 1 of the L293D low
+   digitalWrite(motorPin2, LOW);
+  } else if (r_green <= 16 && r_red <= 15 && r_blue <= 15) {
+    Serial.println("branco");
+  digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
+    delay(50);
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW);
+  } else if (r_green > 30) {
+    Serial.println("Verde");
+  digitalWrite(motorPin1, LOW); // set Input 1 of the L293D low
+    digitalWrite(motorPin2, LOW); // set Input 2 of the L293D high
+  } 
+  Serial.println("-----");
+
+ }
+ void BothColors() {
+Serial.println(String(count) + String("- AMBOS SENSORES"));
+  Serial.println(String("RED: ") + String(r_red && l_red));
+  Serial.println(String("GREEN: ") + String(r_green && l_green));
+  Serial.println(String("BLUE: ") + String(r_blue && l_blue));
+  if (r_green > 30 && l_blue > 30) {
+    Serial.println("180º");
+  digitalWrite(motorPin1, LOW); // set Input 1 of the L293D low
+    digitalWrite(motorPin2, HIGH); // set Input 2 of the L293D high
+  } else if (r_green > 15 && r_green < 30 && l_blue > 15 && l_green > 15 && l_red > 15) {
+    Serial.println("cruzamento");
+    digitalWrite(motorPin1, HIGH); // set Input 1 of the L293D low
+    digitalWrite(motorPin2, LOW); // set Input 2 of the L293D high
+    digitalWrite(motorPin3, HIGH); // set Input 1 of the L293D low
+    digitalWrite(motorPin4, LOW);
+  }
+Serial.println("-----");
+ }
+
